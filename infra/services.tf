2854b92bb691f39b1d6ddffddc0f502f269a5fa8
Otto Lote <otto@lote.no>
2026-05-10 16:16:23 +0200

    Deploy API to GKE

--- infra/services.tf
+++ infra/services.tf
@@ -17,8 +17,20 @@ resource "google_project_service" "cloudresourcemanager" {
   disable_on_destroy = false
 }
 
+resource "google_project_service" "compute" {
+  project            = var.project_id
+  service            = "compute.googleapis.com"
+  disable_on_destroy = false
+}
+
 resource "google_project_service" "container" {
   project            = var.project_id
   service            = "container.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "artifactregistry" {
+  project            = var.project_id
+  service            = "artifactregistry.googleapis.com"
   disable_on_destroy = false
 }
