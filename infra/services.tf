002bf76f8d10d38e87add6321ce8f96d2c009ffc
Otto Lote <otto@lote.no>
2026-05-10 10:40:15 +0200

    chore: setup infrastructure as code with gcp and gitops pipeline, migrate to gcs backend

---
+++ infra/services.tf
@@ -0,0 +1,18 @@
+# Enable required APIs
+resource "google_project_service" "iam" {
+  project            = var.project_id
+  service            = "iam.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "iamcredentials" {
+  project            = var.project_id
+  service            = "iamcredentials.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "cloudresourcemanager" {
+  project            = var.project_id
+  service            = "cloudresourcemanager.googleapis.com"
+  disable_on_destroy = false
+}
